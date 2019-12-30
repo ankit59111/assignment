29bb96f030aa371d4e6cda0907d1d72408e18abf
online <ankitsingh5991111@gmail.com>
2019-12-30 13:32:44 +0530

    home page created

--- react/components/home/home.js
+++ react/components/home/home.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getAllData, getUrl} from "../../../controller/apiList";
+import Header from "../header/header";
+import Carousel from "../carousel/carousel";
+import ProductList from "../productList/productList";
+import Footer from "../footer/footer";
 
-export default class Home extends React.Component{
-    constructor(props){
-        super(props);
-    }
+import "./home.css";
 
-    render() {
-        return(
-            <div>Home route enabled</div>
-        )
-    }
+export default function Home(props) {
+    const [data, loadData] = useState(props.staticContext);
+    const [isLoaded, isLoadedAction] = useState(false)
+
+    useEffect(() => {
+        const url = getUrl(props.match.path);
+        getAllData(url).then(res => {
+            loadData(res);
+            isLoadedAction(true);
+        })
+    }, [data, isLoaded]);
+
+    return (
+        isLoaded || data ?
+            <div className='container'>
+                <Header logo={data[0]['data']['logo']}/>
+                <Carousel imageList={data[0]['data']['carousel']}/>
+                <ProductList list={data[1]['data']}/>
+                <Footer/>
+            </div> :
+            <div></div>
+    )
 }
