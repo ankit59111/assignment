fd112bfafbe8cec85b8e7b5654377f5c84d62753
online <ankitsingh5991111@gmail.com>
2019-12-30 13:33:52 +0530

    routes config

--- react/components/app/app.js
+++ react/components/app/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import Home from "../home/home";
+import ProductDetail from "../productDetail";
+import Page404 from "../404/page404";
+
 export default class App extends React.Component {
     constructor(props) {
         super(props)
@@ -8,10 +11,15 @@ export default class App extends React.Component {
 
     render() {
         return (
+            <React.Fragment>
+
             <Switch>
                 <Route exact render={(props)=> <Home {...props}/>} path="/"/>
+                <Route exact render={(props)=> <ProductDetail {...props}/>} path="/product/:id"/>
+                <Route render={(props)=> <Page404 {...props}/>} path="/*"/>
             </Switch>
+            </React.Fragment>
         )
-    }
+    };
 
-}
+};
