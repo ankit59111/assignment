bda6b4b7b1bf09b6ca24517abae1955db3df37cc
online <ankitsingh5991111@gmail.com>
2019-12-30 13:10:04 +0530

    added server configuration

--- server/index.js
+++ server/index.js
@@ -1,26 +1,37 @@
 import express from 'express';
-import template from '../browser/template';
 import React from 'react';
+import axios from 'axios';
+
+import template from '../browser/template';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import App from "../react/components/app/app";
+import {getUrl,getAllData} from "../controller/apiList";
+
 const server = express();
 
 const PORT = 8001;
 server.use('/assets', express.static('assets'));
 
-server.get('/', (req, res) => {
-    const appHtml = renderToString(
-        <StaticRouter location={req.url} context={{}}>
-            <App />
-        </StaticRouter>
-    );
-    res.send(template({
-        body:appHtml,
-        preloadState:{},
-        title:'Server Side Rendering started'
-    }))
-})
+server.get('/*', (req, res) => {
+    const url = getUrl(req.url);
+    //axios.get('https://node-sample-api.herokuapp.com/api/home').then(res=>console.log(res))
+    getAllData(url).then(response=>{
+        const context = response || {};
+        const appHtml = renderToString(
+            <StaticRouter location={req.url} context={context}>
+                <App />
+            </StaticRouter>
+        );
+        res.send(template({
+            body:appHtml,
+            preloadState:JSON.stringify(context),
+            title:'Server Side Rendering started'
+        }))
+    }).catch(result=>console.log(result))
+
+});
+
 server.listen(PORT, () => {
     console.log('listening at ', PORT)
 })
