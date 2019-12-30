860f8db656020861fb0f1a1ac4387e5ad03f409d
online <ankitsingh5991111@gmail.com>
2019-12-30 13:32:00 +0530

    added footer

---
+++ react/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';
+export default function Footer(){
+
+        return(
+            <footer>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </footer>
+        )
+}
