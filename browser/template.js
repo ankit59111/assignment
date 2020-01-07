14023d053987d54bcf144e3f8f1e6f3783ce0977
online <ankitsingh5991111@gmail.com>
2020-01-07 17:59:53 +0530

    moved stylesheet at top

--- browser/template.js
+++ browser/template.js
@@ -6,6 +6,7 @@ export default ({body, preloadState, title}) => {
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
         <meta name="description" content="Description about page" />
             <title>${title}</title>
+            <link rel="stylesheet" href="/assets/main.css" />
         </head>
         <body>
        
@@ -16,7 +17,7 @@ export default ({body, preloadState, title}) => {
         </script>
         <script src="/assets/1.bundle.js"></script>
         <script src="/assets/bundle.js"></script>
-        <link rel="stylesheet" href="/assets/main.css"/>
+        
      </html>
 `
 }
